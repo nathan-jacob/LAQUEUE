ed2e096e2081fcc7a70f958abd23deaafbd90e9f
nathan-jacob <nathan.jacob9m@gmail.com>
2021-01-31 22:41:28 -0600

    pls help

---
+++ cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/LaQueue.cpp" "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/LaQueue.cpp.o"
+  "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/PQueue.cpp" "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/PQueue.cpp.o"
+  "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/client.cpp" "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/client.cpp.o"
+  "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/lqueue.cpp" "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/lqueue.cpp.o"
+  "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/server.cpp" "/Users/nathanjacob/Downloads/laqueue-nathan-jacob/cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/laqueue_nathan_jacob.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laqueue_nathan_jacob.dir/LaQueue.cpp.o"
+  "CMakeFiles/laqueue_nathan_jacob.dir/PQueue.cpp.o"
+  "CMakeFiles/laqueue_nathan_jacob.dir/client.cpp.o"
+  "CMakeFiles/laqueue_nathan_jacob.dir/lqueue.cpp.o"
+  "CMakeFiles/laqueue_nathan_jacob.dir/server.cpp.o"
+  "laqueue_nathan_jacob"
+  "laqueue_nathan_jacob.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laqueue_nathan_jacob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
